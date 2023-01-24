b709f1b8fc557f58ca21b589dcfbbe5a15441358
Stefan Senk <stefan.senk@stud.fh-campuswien.ac.at>
2023-01-24 22:05:49 +0100

    DESEQ2 script including gene_name in results.csv

--- Deseq2_script.R
+++ Deseq2_script.R
@@ -24,20 +24,20 @@ Sample_info
 # find the first file of the Sample info file
 first_file <- paste(Sample_info$samplename[1],".counts.tsv",sep = "")
 
+#prepare raw count table with first file
+first_file <- read.table(file = first_file , sep="\t", header=TRUE)
+raw_counts <- first_file[,c(1,8)]
+colnames(raw_counts) <- c("geneID", Sample_info$samplename[1]); colnames(raw_counts)
+
 #creating gene annotation for later
 annotation <- first_file[,c(1,7)]
 colnames(annotation) <- c("geneID", "gene_name")
 
-#prepare raw count table with first file
-raw_counts <- read.table(file = first_file , sep="\t", header=TRUE)
-raw_counts <- raw_counts[,c(1,7)]
-colnames(raw_counts) <- c("geneID", Sample_info$samplename[1]); colnames(raw_counts)
-
 #append all files according to Sample_Info.txt
 for (i in 2:length(Sample_info$samplename)){
-  next_file = match(paste(Sample_info$samplename[i],".counts.tsv",sep = ""), feature_count_files);next_file
+  next_file = match(paste(Sample_info$samplename[i],".counts.tsv",sep = ""), feature_count_files);next_file #find index in feature_count_files
   tmp <- read.table(file = feature_count_files[next_file], sep="\t", header=TRUE); 
-  tmp <- tmp[,c(1, 7)]
+  tmp <- tmp[,c(1, 8)]
   colnames(tmp) <- c("geneID", Sample_info$samplename[i])
   if (any(duplicated(tmp$GeneID))){
     stop("Duplicated feature names in ", Sample_info$samplename[i], ": ", 
@@ -93,7 +93,7 @@ dds <- DESeq(dds)
   #       of genes which will have an adjusted p value below a given FDR cutoff, alpha (Default = 0.1"
 
 res05 <- results(dds, alpha = 0.05)
-
+res05$gene_name <- mcols(dds)$gene_name
 #Order gene expression table by adjusted p value (Benjamini-Hochberg FDR method)
 res05 <- res05[order(res05$padj),]
 
